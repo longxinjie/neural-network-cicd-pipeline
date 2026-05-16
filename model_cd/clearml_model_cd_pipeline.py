19a6b24fce66c41f59e3f4f9deb0c47b898f5d75
xinjie-long <xinjie.long.2023@scis.smu.edu.sg>
2026-05-16 18:21:01 +0200

    fixing docker issues

--- model_cd/clearml_model_cd_pipeline.py
+++ model_cd/clearml_model_cd_pipeline.py
@@ -1,23 +1,16 @@
-from clearml import Task, PipelineController
+from clearml import PipelineController
 
 pipe = PipelineController(
     project="Neural-Network-CICD",
     name="Remote Model Continuous Delivery Pipeline",
-    version="1.0.0",
+    version="1.0.1",
     add_pipeline_tags=True,
 )
 
 pipe.set_default_execution_queue("default")
 
 pipe.add_step(
-    name="train_model",
-    base_task_project="Neural-Network-CICD",
-    base_task_name="Local CNN Training",
-)
-
-pipe.add_step(
     name="validate_checkpoint",
-    parents=["train_model"],
     base_task_project="Neural-Network-CICD",
     base_task_name="Validate Checkpoint",
 )
@@ -34,6 +27,13 @@ pipe.add_step(
     parents=["register_model"],
     base_task_project="Neural-Network-CICD",
     base_task_name="Deploy Model to Staging",
+)
+
+pipe.add_step(
+    name="validate_endpoint",
+    parents=["deploy_model"],
+    base_task_project="Neural-Network-CICD",
+    base_task_name="Validate Deployment Endpoint",
 )
 
 print("Submitting remote Model CD pipeline to ClearML queue...")
